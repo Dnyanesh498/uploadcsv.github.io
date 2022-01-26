2245c0cffe1145d6d2448a3a58a6cdb12367dcc5
Dnyanesh498 <84060619+Dnyanesh498@users.noreply.github.com>
2022-01-26 13:27:09 +0530

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -19,34 +19,7 @@ module.exports.uploadFile = (req, res) => {
         return res.redirect("back");
     })
 }
-// module.exports.viewFile = async (req, res) => {
-//     let headers = [];
-//     let dataArray = [];
-//     // or rather use id here
-//     let fileId = req.query.fileId;
-//     console.log(fileId) ;
-//     let fileObj = await File.findById(fileId);
-//     await fs.createReadStream(path.join(__dirname, "..", fileObj.filePath))
-//         .pipe(csv())
-//         .on('headers', (header_arr) => {
 
-//             headers = header_arr;
-//         })
-//         .on('data', (row) => {
-//             // console.log(row) ;
-//             dataArray.push(row);
-//         })
-//         .on('end', () => {
-//             console.log("csv file successfully processed");
-//             // console.log(headers);
-//             // console.log(dataArray)
-//             res.render("viewFile", {
-//                 headers: headers
-//                 , dataArray: dataArray
-//             })
-//         });
-
-// }
 module.exports.viewFile = async (req, res) => {
     let headers = [];
     let dataArray = [];
